43c2922f032a0f60778f0fb669b078304f1a10af
Stevie de Gala <steviedegala@Stevies-Mac-mini.local>
2026-06-11 14:08:15 -0600

    Add static export output for Cloudflare Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {},
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
